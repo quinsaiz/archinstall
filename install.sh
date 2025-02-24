d160ba53a14ca96b6494912139ace10510b374bf
Edward <edgood228@gmail.com>
2025-02-24 14:41:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -134,41 +134,41 @@ else
 fi
 
 # Вибір відеокарти
-echo "${BLUE}=== Виберіть драйвери GPU ===${RESET}"
-echo "1) AMD (mesa) 2) NVIDIA (nvidia-open-dkms) 3) AMD+NVIDIA (гібрид)"
-read -p "${YELLOW}Вибір: ${RESET}" GPU
+echo "${BLUE}=== Choose a GPU driver ===${RESET}"
+echo "1) AMD (mesa) 2) NVIDIA (nvidia-open-dkms) 3) AMD+NVIDIA (notebook)"
+read -p "${YELLOW}Choice: ${RESET}" GPU
 case $GPU in
     1) GPU_PKGS="mesa vulkan-radeon vulkan-icd-loader"; MODULES="amdgpu" ;;
     2) GPU_PKGS="nvidia-open-dkms nvidia-utils nvidia-settings vulkan-icd-loader"; MODULES="nvidia nvidia_modeset nvidia_uvm nvidia_drm";;
     3) GPU_PKGS="mesa vulkan-radeon vulkan-icd-loader xf86-video-amdgpu nvidia-open-dkms nvidia-utils nvidia-prime nvidia-settings"; MODULES="amdgpu";;
-    *) echo "${RED}Невірний вибір, встановлюємо mesa${RESET}"; GPU_PKGS="mesa vulkan-radeon vulkan-icd-loader"; MODULES="amdgpu" ;;
+    *) echo "${RED}Wrong choice, install mesa${RESET}"; GPU_PKGS="mesa vulkan-radeon vulkan-icd-loader"; MODULES="amdgpu" ;;
 esac
 
 # Вибір графічного середовища
-echo "${BLUE}=== Виберіть графічне середовище ===${RESET}"
-echo "1) Gnome 2) Gnome Minimal 3) KDE 4) KDE Minimal 5) Без графічного середовища"
-read -p "${YELLOW}Вибір: ${RESET}" DE
+echo "${BLUE}=== Choose a graphics environment ===${RESET}"
+echo "1) Gnome 2) Gnome Minimal 3) KDE 4) KDE Minimal 5) None"
+read -p "${YELLOW}Choice: ${RESET}" DE
 case $DE in
     1) DE_PKGS="gnome gdm pipewire-jack"; DE_SERVICE="gdm" ;;
     2) DE_PKGS="gnome-shell gdm nautilus gnome-control-center gnome-settings-daemon gnome-session xdg-desktop-portal-gnome gvfs gvfs-mtp gnome-keyring gnome-tweaks dconf-editor gnome-terminal gnome-themes-extra gvfs-smb gvfs-nfs pipewire-jack"; DE_SERVICE="gdm" ;;
     3) DE_PKGS="plasma kde-applications sddm"; DE_SERVICE="sddm" ;;
     4) DE_PKGS="plasma-desktop sddm"; DE_SERVICE="sddm" ;;
     5) DE_PKGS=""; DE_SERVICE="" ;;
-    *) echo "${RED}Невірний вибір, без графічного середовища${RESET}"; DE_PKGS=""; DE_SERVICE="" ;;
+    *) echo "${RED}Wrong choice, none graphics environment${RESET}"; DE_PKGS=""; DE_SERVICE="" ;;
 esac
 
 # Встановлення базових пакетів
-echo "${YELLOW}Встановлення базових пакетів...${RESET}"
+echo "${YELLOW}Installing basic packages...${RESET}"
 pacstrap -i /mnt base base-devel $KERNEL_PKG linux-firmware $UCODE $FS_TOOLS nano networkmanager $GPU_PKGS $DE_PKGS
 
 # Генерація fstab
-echo "${YELLOW}Генерація fstab...${RESET}"
+echo "${YELLOW}Generating fstab...${RESET}"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Вибір локалізації
-echo "${BLUE}=== Виберіть мову ===${RESET}"
-echo "1) Українська 2) Англійська"
-read -p "${YELLOW}Вибір: ${RESET}" LANG
+echo "${BLUE}=== Select a language ===${RESET}"
+echo "1) Ukrainian 2) English"
+read -p "${YELLOW}Choice: ${RESET}" LANG
 if [ "$LANG" == "1" ]; then
     LOCALE="en_US.UTF-8 UTF-8\nuk_UA.UTF-8 UTF-8"
     LANG_CONF="LANG=uk_UA.UTF-8"
@@ -180,9 +180,9 @@ else
 fi
 
 # Вибір os-prober
-echo "${BLUE}=== Виявлення інших ОС ===${RESET}"
-echo "Встановити os-prober? 1) Так 2) Ні"
-read -p "${YELLOW}Вибір: ${RESET}" OSPROBER
+echo "${BLUE}=== Detecting other OS ===${RESET}"
+echo "Install os-prober? 1) Так 2) Ні"
+read -p "${YELLOW}Choice: ${RESET}" OSPROBER
 if [ "$OSPROBER" == "1" ]; then
     OSPROBER_PKG="os-prober"
 else
@@ -190,7 +190,7 @@ else
 fi
 
 # chroot і налаштування
-echo "${YELLOW}Налаштування системи в chroot...${RESET}"
+echo "${YELLOW}Setting up the system in chroot...${RESET}"
 arch-chroot /mnt /bin/bash <<EOF
 # Часовий пояс
 ln -sf /usr/share/zoneinfo/Europe/Kyiv /etc/localtime
@@ -227,7 +227,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 if [ "$GPU" == "3" ]; then
     mkdir -p /etc/X11/xorg.conf.d
     echo -e 'Section "OutputClass"\n    Identifier "nvidia"\n    MatchDriver "nvidia-drm"\n    Driver "nvidia"\n    Option "PrimaryGPU" "no"\nEndSection' > /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf
-    echo "${GREEN}Переконайтеся, що /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf має правильний вміст:${RESET}"
+    echo "${GREEN}Make sure that /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf has the correct contents:${RESET}"
     cat /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf
 fi
 
@@ -241,7 +241,7 @@ sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 systemctl enable NetworkManager
 
 # Додаткові пакети
-pacman -S --noconfirm firefox firefox-i18n-uk qbittorrent vlc neofetch btop gnome-browser-connector gnome-tweaks bash-completion adw-gtk-theme steam
+# pacman -S --noconfirm firefox firefox-i18n-uk qbittorrent vlc neofetch btop gnome-browser-connector gnome-tweaks bash-completion adw-gtk-theme steam
 
 # Активація графічного середовища
 if [ -n "$DE_SERVICE" ]; then
@@ -251,8 +251,8 @@ fi
 EOF
 
 # Завершення
-echo "${GREEN}Встановлення завершено! Розмонтування та перезавантаження...${RESET}"
+echo "${GREEN}Installation complete! Unmount and reboot...${RESET}"
 umount -R /mnt
-echo "${YELLOW}Вийміть носій і натисніть Enter для перезавантаження${RESET}"
+echo "${YELLOW}Eject media and press Enter to reboot${RESET}"
 read
 reboot
